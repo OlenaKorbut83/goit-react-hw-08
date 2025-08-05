554c758bd063e6488ab207de4a4a03c7420ac9f2
OlenaKorbut83 <olenakorbut83@gmail.com>
2025-08-05 22:26:59 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+import ContactForm from "./components/ContactForm/ContactForm";
+import ContactList from "./components/ContactList/ContactList";
+import SearchBox from "./components/SearchBox/SearchBox";
+
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { fetchContacts } from "./redux/contactsOps";
+
+function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <div className="app">
+      <h1 className="app_title">Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,33 @@
+import { useSelector } from "react-redux";
+import Contact from "../Contact/Contact";
+import s from "./ContactList.module.css";
+
+import {
+  selectError,
+  selectFilteredContacts,
+  selectLoading,
+} from "../../redux/contactsSlice";
+
+const ContactList = () => {
+  const loading = useSelector(selectLoading);
+  const error = useSelector(selectError);
+  const visibleContacts = useSelector(selectFilteredContacts);
+
+  return (
+    <>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      <ul className={s.contacts_list}>
+        {visibleContacts.map((item) => {
+          return (
+            <li className={s.contact_item} key={item.id}>
+              <Contact id={item.id} name={item.name} phone={item.number} />
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default ContactList;
